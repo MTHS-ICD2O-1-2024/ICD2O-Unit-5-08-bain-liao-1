c0f5890052b05f15b94c859709a1da5cc752d38a
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-05-09 17:17:28 +0000

    created program for division using repeated subtraction

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bain Liao
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates division using repeated subtraction.
+ */
+function calculateDivisionWithSubtraction() {
+  const userDividend = parseInt(document.getElementById("user-dividend").value)
+  const userDivisor = parseInt(document.getElementById("user-divisor").value)
+  let remainder = userDividend
+  let counter = 0
+
+  while (true) {
+    if (remainder < userDivisor) {
+      break
+    }
+    counter ++
+    remainder -= userDivisor
+  }
+
+  document.getElementById("result").innerText =
+    userDividend + " ÷ " + userDivisor + " = " + counter + " and the remainder is: " + remainder
+}
